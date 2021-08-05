12ae14ae4cdd35a610b9ec705323d5c8f6b62342
dmircea <dmircea@csu.fullerton.edu>
2021-08-04 21:22:06 -0700

    Completed component section.

--- src/App.js
+++ src/App.js
@@ -45,31 +45,47 @@ function App() {
         My Hacker Stories
       </h1>
 
-      <label htmlFor="search"> Search: </label>
-      <input id="search" type="text"/>
+      <Search />
+
       <hr />
 
       {/* Example comment in JSX! */}
 
-      <ul>
-        {
-          list.map(function (item) {
-            return (
-                <li key={item.objectID}>
-                  <span>
-                    {/*It seems that if url is null, no url is attached.*/}
-                    <a href={item.url}>{item.title}</a>
-                    }
-                  </span>
-                  <span> {item.author} </span>
-                  <span> {item.num_comments} </span>
-                  <span> {item.points} </span>
-                </li>
-              );
-          })
-        }
-      </ul>
+      <List />
 
+    </div>
+  );
+}
+
+function List()
+{
+  return(
+    <ul>
+      {
+        list.map(function (item) {
+          return (
+              <li key={item.objectID}>
+                <span>
+                  {/*It seems that if url is null, no url is attached.*/}
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span> {item.author} </span>
+                <span> {item.num_comments} </span>
+                <span> {item.points} </span>
+              </li>
+            );
+        })
+      }
+    </ul>
+  );
+}
+
+function Search()
+{
+  return (
+    <div>
+      <label htmlFor="search"> Search: </label>
+      <input id="search" type="text"/>
     </div>
   );
 }
